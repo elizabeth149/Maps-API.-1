bdd64ff443d281e8e1c46c73a47ef994effa529e
HehexOne <werty.mn2@yandex.ru>
2020-02-14 18:35:39 +0400

    Maps API. Часть №2

---
+++ ttt.py
@@ -0,0 +1,44 @@
+import sys
+import requests
+import pygame
+
+address_ll = ",".join(sys.argv[1:3])
+size = 3
+response = None
+
+def sizze(r):
+    global address_ll
+    global response
+    sp = f"https://static-maps.yandex.ru/1.x/?ll={address_ll}&z={r}&l=sat"
+    response = requests.get(sp)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+sizze(size)
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            if 1 <= size <= 18:
+                if size < 19:
+                    size += 1
+                    sizze(size)
+                elif size == 19:
+                    size = 1
+        if keys[pygame.K_DOWN]:
+            if 1 <= size <= 18:
+                if size > 0:
+                    size -= 1
+                    sizze(size)
+                elif size == 0:
+                    size = 18
